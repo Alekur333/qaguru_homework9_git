4eea8e5b209a406775c69993fe2ad00588e4f1a4
Dmitriy Kochurov <kraver90@gmail.com>
2021-11-21 19:58:55 +0500

    add data in TestData

--- src/test/java/kur/alexei/testData/TestData.java
+++ src/test/java/kur/alexei/testData/TestData.java
@@ -1,4 +1,8 @@
 package kur.alexei.testData;
 
 public class TestData {
+    // точно не хватает в проекте TestData
+    public static String UPLOAD_PAGE_FILE_UPLOAD = "https://www.file-upload.com/";
+    public static String DOWNLOAD_FILE_TXT = "https://www.mediafire.com/file/n72j4t7n4n2dsfe/testfile.txt/file";
+    public static String DOWNLOAD_FILE_PDF = "https://www.tutorialspoint.com/selenium/selenium_pdf_version.htm";
 }
