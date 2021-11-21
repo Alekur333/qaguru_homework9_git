c18f09f257deb3824f43464281d1f9a62a62313e
Alekur333 <amastar963@gmail.com>
2021-11-21 17:52:42 +0500

    added "List.of"

--- src/test/java/kur/alexei/tests/GitTest.java
+++ src/test/java/kur/alexei/tests/GitTest.java
@@ -2,14 +2,20 @@ package kur.alexei.tests;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 public class GitTest {
 
     @Test
     void gitTest() {
-        System.out.println("newBrach");
+        String listInList = "a, b, c";
+
         // commit 1
         // commit 2
         // commit 3
+        java.util.List<String> strings = List.of(listInList);
+        System.out.println(strings);
+
 
     }
 
