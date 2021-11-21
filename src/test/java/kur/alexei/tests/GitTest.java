d03b6bdc4c9c77ad0e1ee4be95e07c01f839e0e8
Alekur333 <amastar963@gmail.com>
2021-11-21 13:46:43 +0500

    added gradle/wrapper in .gitignore

---
+++ src/test/java/kur/alexei/tests/GitTest.java
@@ -0,0 +1,14 @@
+package kur.alexei.tests;
+
+import org.junit.jupiter.api.Test;
+
+public class GitTest {
+
+    @Test
+    void gitTest() {
+        System.out.println("newBrach");
+
+    }
+
+
+}
